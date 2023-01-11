07c3189fb290dd0c9de43e02b82f866016ad3272
George Pouleres <georgepouleres@gmail.com>
2023-01-11 12:25:15 +0200

    Added test data on Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+/***
+ * created by gp
+ */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MyList list = new MyList();
+
+        list.printHead();
+
+        System.out.println("The list length is: " + list.getLength());
+
+        System.out.println("Removed " + list.removeLast());
+
+
+        list.addFirst(10);
+        list.addFirst(15);
+
+        System.out.println("The list length is: " + list.getLength());
+
+        list.addFirst(30);
+        list.addLast(9);
+        list.addLast(5);
+
+        System.out.println("The list length is: " + list.getLength());
+
+
+        System.out.println("Removed " + list.removeLast());
+
+        list.printHead();
+
+        list.printList();
+
+        System.out.println("The list length is: " + list.getLength());
+
+        list.addInPlace(3 , 100);
+
+        list.printHead();
+
+        list.printList();
+
+        System.out.println("The list length is: " + list.getLength());
+
+
+
     }
 }
