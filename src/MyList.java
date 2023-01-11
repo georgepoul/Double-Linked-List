ea67eb9f8aaed02185dc77821f2f258d9fb6bb1a
George Pouleres <georgepouleres@gmail.com>
2023-01-11 12:23:56 +0200

    Added MyList Class

---
+++ src/MyList.java
@@ -0,0 +1,210 @@
+/***
+ * created by gp
+ */
+public class MyList {
+
+    private Node head;
+
+    private Node tail;
+
+    private int length;
+
+    public MyList() {
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    /***
+     * prints the nodes address
+     */
+    public void node_print(Node node) {
+
+        System.out.println(node.getData());
+    }
+
+    public void printHead() {
+
+        if (this.head == null) {
+            System.out.println("The head is: null");
+        } else {
+            System.out.println("The head is: " + head.getData());
+        }
+    }
+
+    /***
+     * prints the hole list
+     */
+    public void printList() {
+
+        Node current = head;
+
+        while (current != null) {
+
+            node_print(current);
+
+            current = current.getNext();
+        }
+    }
+
+    /***
+     * add a node in the first place of my list
+     * @param in data that I want to add to my list
+     */
+    public void addFirst(int in) {
+
+        Node tmp = new Node(in);
+
+        if (length > 0) {
+
+            tmp.setNext(this.head);
+
+            head.setPrevious(tmp);
+
+        }else {
+
+            tail = tmp;
+        }
+
+        head = tmp;
+
+        length++;
+    }
+
+    /***
+     * add a node in the last place of my list
+     * @param in data that I want to add to my list
+     */
+    public void addLast(int in) {
+
+        Node tmp = new Node(in);
+
+        if (length == 0) {
+
+            tail = head = tmp;
+
+        } else{
+
+            tmp.setPrevious(tail);
+
+            tail.setNext(tmp);
+
+            tail = tmp;
+        }
+
+        this.length++;
+
+    }
+
+    /***
+     * remove the last node of the list
+     * @return -1 if the list is empty or the data of the node I removed
+     */
+    public int removeLast() {
+
+        Node current = head;
+
+        int result = -1;
+
+        if (length == 0){
+
+            System.out.println("List is empty");
+
+        }else if (length == 1){
+
+            result = head.getData();
+
+            head = tail = null;
+
+            this.length --;
+
+        }else {
+
+            result = tail.getData();
+
+            tail = tail.getPrevious();
+
+            tail.setNext(null);
+
+            length --;
+
+        }
+
+        return result;
+    }
+
+    /***
+     * Adds a value in a specific position
+     * if the position is in upper out of the list then adds zero data nudes and the value in the position we want
+     * if the position is not valid print a message
+     * @param position the position for the value to be added
+     * @param value the value to be added
+     */
+    public void addInPlace(int position, int value) {
+
+        Node current = head;
+
+        int isInplace = 0;
+
+        if (position == 0) {
+
+            addFirst(value);
+
+        } else if (position > 0 && position < this.length) {
+
+            while (isInplace != position - 1) {
+
+                current = current.getNext();
+
+                isInplace++;
+            }
+
+            Node newNode = new Node(value);
+
+            Node tmp = current.getNext();
+
+            current.setNext(newNode);
+
+            newNode.setNext(tmp);
+
+            System.out.println("The value: " + value + " added in position: " + isInplace);
+
+        }else if (position >= this.length){
+
+            for (int i = this.length - 1; i < position - 1; i ++){
+
+                addLast(0);
+            }
+
+            addLast(value);
+
+            System.out.println("The value: " + value + " added in position: " + position);
+
+        }else{
+
+            System.out.println("The position you have insert is not valid. Please try again");
+
+        }
+    }
+}
